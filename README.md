9d888318cef73586c5ad9db4560937d348dce7de
ylqylqylq0328 <ylqylqylq@gmail.com>
2020-10-17 12:07:43 -0400

    Updated

--- README.md
+++ README.md
@@ -6,59 +6,78 @@ Test Simple Calculator.
     reporting.baseDir = "my-reports"
     testResultsDirName = "$buildDir/my-test-results"
 ### Test cases
-Totally there are 30 test cases: 24 are successful and 6 failed. Please see "Bug" section for bugs' analysis.
+Totally there are 43 test cases: 34 are successful and 9 failed. Please see "Bug" section for bugs' analysis.
 
-    Basic Operation
-    Order Of Operation
-    Decimal Operation
-    Zero Operation
-    Big Data Operation
-    Del Operation
-    Error Operation
-    Multiple Equality Sign
-    Expression Display
+    TC1 Key Response
+    TC2 Basic Operation
+    TC3 Order Of Operation
+    TC4 Negative Number
+    TC5 Decimal Result
+    TC6 Repeating Decimal Result
+    TC7 Zero Operation
+    TC8 Big Data Operation
+    TC9 Del Key Operation
+    TC10 Invalid Operation
+    TC11 Multiple Equality Sign
+    TC12 Expression Display
 
 ### Bug
-1 Decimal Operation Abnormal.
+1 Repeating Decimal Operation Abnormality.
 
     Case No | Expression | Expected Result   | Actual Result     | Result
     1       | 1/3*2=     | 0.6666666666666667| 0.6666666666666666| Failure
     2       | 1+1/3*2=   | 1.6666666666666667| 1.6666666666666665| Failure
     3       | 100+1/3*2= | 100.66666666666667| 100.66666666666667| Pass
- 
+
+The same rule should be used when we round a repeating decimal.
+
 2 Use of inappropriate error messages
 
     Case No | Expression | Expected Result   | Actual Result     | Result
     1       | 1/0  =     | Undefined         | Infinity          | Failure
     
-3 Error Operation
+3 Big Data Operation
+
+    Case No | Expression            | Expected Result    | Actual Result      | Result
+    1       | 123456789012345*2=    | 246913578024690    | 246913578024690    | Pass
+    2       | 1234567890123456*2=   | 2469135780246912   | 2469135780246912   | Pass
+    3       | 12345678901234567*2=  | 24691357802469134  | 24691357802469136  | Failure
+    4       | 123456789012345678*2= | 246913578024691356 | 246913578024691360 | Failure
+
+The answer is incorrect when it contains more than 16 digits. To fix this issue, we should use 
+the scientific notation to round the number.
+
+4 Invalid Operation
 
     Case No | Expression | Expected Result   | Actual Result     | Result
     1       | 1+-=       | ERR               | ERR               | Pass
     2       | +-1=       | ERR               | -1                | Failure
     
-4 Multiple Equality Sign
+5 Multiple Equality Sign
 
     Case No | Expression | Expected Result | Actual Result | Expected Expression |Actual Expression |Result
     1       | 1+1=2+2=   | 4               | 17            | 3+3                 |1+13+3            |Failure
     
 The reason is that the input needs to be cleared after the result appears and before we can perform next operations.
 
-5 Expression Display
+6 Expression Display
 
     Case No | Expression | Expected Expression | Actual Expression | Result
     1       | 1+05=      | 1+5                 | 1+05              | Failure
+    1       | 05+1=      | 5+1                 | 05+1              | Failure
     
 We should change the input's formatting when the first digit of a number is zero.    
 
-6 Other Bugs
+7 Other Bugs
   
 <1> It should provide a "CLEAR" key to clear all input, otherwise the user has to click the "DEL" key multiple times.
 
 <2> If the result of the equation is too long, the first digit of the answer overwrites the equality symbol.
  For example, this happens when the input is "1/3=". This can be fixed if we can decrease the maximum digits of the output by 1.
 
-
+<3> If the input expression is more than 18 digits and symbols, the next digits and symbols cannot be seen. To fix this issue,
+we should set the maximum input to 18 digits and symbols, or to decrease the size of each digit or symbol 
+when there are more than 18 of them.
 
 
 
--- src/test/java/com/testCalculator/TestCase/CalculatorTest.java
+++ src/test/java/com/testCalculator/TestCase/CalculatorTest.java
@@ -17,7 +17,7 @@ public class CalculatorTest extends AbstractTest {
             "1234567890+-*/,  '', 1234567890+-×/",
             "1234567890+-*/D, '', 1234567890+-×",
     })
-    public void testKeyResponse(String input, String result, String input_expression) {
+    public void testTC1_KeyResponse(String input, String result, String input_expression) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.Actions(input);
         assertAll(
@@ -33,7 +33,20 @@ public class CalculatorTest extends AbstractTest {
             "1*1=, 1",
             "1/1=, 1"
     })
-    public void testBasicOperation(String input, String result) {
+    public void testTC2_BasicOperation(String input, String result) {
+        CalculatorPage calculatorPage = new CalculatorPage(driver);
+        calculatorPage.Actions(input);
+        assertEquals(result, calculatorPage.getOutput());
+    }
+
+    @ParameterizedTest(name = "{index} => input={0}, result={1}")
+    @CsvSource({
+            "-5+3=, -2",
+            "-5*3=, -15",
+            "-6/3=, -2",
+            "-6-3=, -9",
+    })
+    public void testTC3_NegativeNumber(String input, String result) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.Actions(input);
         assertEquals(result, calculatorPage.getOutput());
@@ -48,9 +61,23 @@ public class CalculatorTest extends AbstractTest {
             "1+6/3=, 3",
             "1-6/3=, -1",
             "6/3/2=, 1",
-
+            "2*3*6=, 36",
+            "6/3*2=, 4",
+            "6*2/3=, 4",
     })
-    public void testOrderOfOperation(String input, String result) {
+    public void testTC4_OrderOfOperation(String input, String result) {
+        CalculatorPage calculatorPage = new CalculatorPage(driver);
+        calculatorPage.Actions(input);
+        assertEquals(result, calculatorPage.getOutput());
+    }
+
+    @ParameterizedTest(name = "{index} => input={0}, result={1}")
+    @CsvSource({
+            "1/2=,   0.5",
+            "1+1/4=, 1.25",
+            "1/4*4=, 1",
+    })
+    public void testTC5_DecimalResult(String input, String result) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.Actions(input);
         assertEquals(result, calculatorPage.getOutput());
@@ -62,7 +89,7 @@ public class CalculatorTest extends AbstractTest {
             "1+1/3*2=,   1.6666666666666667",
             "100+1/3*2=, 100.66666666666667",
     })
-    public void testDecimal(String input, String result) {
+    public void testTC6_RepeatingDecimalResult(String input, String result) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.Actions(input);
         assertEquals(result, calculatorPage.getOutput());
@@ -74,7 +101,7 @@ public class CalculatorTest extends AbstractTest {
             "0/1=, 0",
             "1*0=, 0",
     })
-    public void testZero(String input, String result) {
+    public void testTC7_ZeroOperation(String input, String result) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.Actions(input);
         assertEquals(result, calculatorPage.getOutput());
@@ -82,10 +109,12 @@ public class CalculatorTest extends AbstractTest {
 
     @ParameterizedTest(name = "{index} => input={0}, result={1}")
     @CsvSource({
-            "123456789*987654321=, 121932631112635260",
-            "123456789/987654321=, 0.1249999988609375",
+            "123456789012345*2=, 246913578024690",
+            "1234567890123456*2=, 2469135780246912",
+            "12345678901234567*2=, 24691357802469134",
+            "123456789012345678*2=, 246913578024691356",
     })
-    public void testBigData(String input, String result) {
+    public void testTC8_BigDataOperation(String input, String result) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.Actions(input);
         assertEquals(result, calculatorPage.getOutput());
@@ -95,7 +124,7 @@ public class CalculatorTest extends AbstractTest {
     @CsvSource({
             "1+1D1=, 2, 1+1"
     })
-    public void testDelOperation(String input, String result, String input_expression) {
+    public void testTC9_DelKeyOperation(String input, String result, String input_expression) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.Actions(input);
         assertAll(
@@ -111,7 +140,7 @@ public class CalculatorTest extends AbstractTest {
             "*/1=, ERR",
             "*/1=, ERR",
     })
-    public void testErrorOperation(String input, String result) {
+    public void testTC10_InvalidOperation(String input, String result) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.Actions(input);
         assertEquals(result, calculatorPage.getOutput());
@@ -121,7 +150,7 @@ public class CalculatorTest extends AbstractTest {
     @CsvSource({
             "1+1=3+3=, 6, 3+3",
     })
-    public void testMultipleEqualitySign(String input, String result, String input_expression) {
+    public void testTC11_MultipleEqualitySign(String input, String result, String input_expression) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.Actions(input);
         assertAll(
@@ -133,8 +162,9 @@ public class CalculatorTest extends AbstractTest {
     @ParameterizedTest(name = "{index} => input={0}, result={1}")
     @CsvSource({
             "1+05=, 6, 1+5",
+            "05+1=, 6, 1+5"
     })
-    public void testExpression(String input, String result, String input_expression) {
+    public void testTC12_ExpressionDisplay(String input, String result, String input_expression) {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.Actions(input);
         assertAll(
